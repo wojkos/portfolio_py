eefad0e0ab7c83ee93109854834c2e6d1740f7d2
wojtek <wojtek.kostanski@gmail.com>
2019-02-25 23:02:44 +0100

    returning definition only if word exist

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,6 +3,10 @@ import json
 data = json.load(open("../dictionary/data.json"))
 
 def translate(word):
-  return data[word]
+  if word in data:
+    return data[word]
+  else:
+    return "The word doesn't exist"
 
 print(translate("rain"))
+print(translate("Noga"))
