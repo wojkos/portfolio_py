5b6cf21544effa25e851ec4152807677ddf83cb8
wojtek <wojtek.kostanski@gmail.com>
2019-02-25 23:15:11 +0100

    get user input

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,10 +3,12 @@ import json
 data = json.load(open("../dictionary/data.json"))
 
 def translate(word):
+  word = word.lower()
   if word in data:
     return data[word]
   else:
     return "The word doesn't exist"
 
-print(translate("rain"))
-print(translate("Noga"))
+word = input('Give me word:')
+
+print(translate(word))
