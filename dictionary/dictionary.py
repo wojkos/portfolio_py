6bc0d6fec20b809430b9101bd32c7e7ba642ae65
wojtek <wojtek.kostanski@gmail.com>
2019-02-25 23:55:55 +0100

    give proposition of word

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("../dictionary/data.json"))
 
@@ -6,6 +7,18 @@ def translate(word):
   word = word.lower()
   if word in data:
     return data[word]
+  elif len(get_close_matches(word, data.keys())) > 0:
+    close_match_list = get_close_matches(word, data.keys(), 3)
+    print("Do you mean?")
+    [print("%s) %s"%(i, a)) for i, a in enumerate(close_match_list)]
+    print("3) something else")
+    choose = input("Choose option:")
+    if choose in ["0", "1", "2"]:
+      return data[close_match_list[int(choose)]]
+    elif choose == "3":
+      return "Something else... Sorry hear that. Try again"
+    else:
+      return "Invalid input. Try again"
   else:
     return "The word doesn't exist"
 
