dc062ee6807b14e3f054f679252737afdb5342ca
wojtek <wojtek.kostanski@gmail.com>
2019-02-27 22:23:00 +0100

    add countries

--- map/map.py
+++ map/map.py
@@ -28,6 +28,12 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
     fg.add_child(folium.CircleMarker(location=[lt, ln],radius=6, popup=folium.Popup(iframe),fill_color = pointer_color(el), color='grey', fill_opacity=0.6))
  
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+fg.add_child(folium.GeoJson(data=(open("poland.json", "r", encoding="utf-8-sig").read())))
+
 map.add_child(fg)
 
 map.save("map.html")
