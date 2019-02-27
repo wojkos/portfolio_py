c2e71eecfd6ede5bcd0863b9dd65bb316a9e81fe
wojtek <wojtek.kostanski@gmail.com>
2019-02-27 21:41:04 +0100

    pointer color from elevation

--- map/map.py
+++ map/map.py
@@ -12,12 +12,21 @@ html = """
 Height: %s m
 """
  
+def pointer_color(elevation):
+  if elevation < 1000:
+    return "green"
+  elif 100 <= elevation < 3000:
+    return "orange"
+  else:
+    return "red"
+
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name = "My Map")
  
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "green")))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = pointer_color(el))))
  
 map.add_child(fg)
 
